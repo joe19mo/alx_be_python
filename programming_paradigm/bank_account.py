0a8fd42dafe3de077297eff9f487b0a401b77d90
yousef mohamed <ymobos8@gmail.com>
2025-10-08 17:59:59 +0300

    bank account

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -21,5 +21,5 @@ class BankAccount:
             return False
 
     def display_balance(self):
-       
-        print(f"Current Balance: ${self.__account_balance}")
+    
+     print(f"Current Balance: ${self.__account_balance:.2f}")
